89081ec79460bb11eaa1819618f0c51858e0b872
wpjscc <wpjscc@gmail.com>
2025-07-20 15:26:06 +0800

    example

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -9,8 +9,25 @@ class ExampleController
 {
     public function index(ServerRequestInterface $request)
     {
-        return Response::plaintext(
-            "Hello wörld!\n"
-        );
+        // 获取请求头
+        $serverParams = $request->getServerParams();
+        // 获取请求参数
+        $queryParams = $request->getQueryParams();
+        // Content-Type: application/x-www-form-urlencoded or Content-Type: multipart/form-data
+        $parsedBody = $request->getParsedBody();
+        $name = $parsedBody['name'] ?? 'World';
+
+        // Content-Type: application/json
+        $data = json_decode((string)$request->getBody());
+
+        return Response::json([
+            'code' => 0,
+            'msg' => 'success',
+            'data' => [
+                'serverParams' => $serverParams,
+                'queryParams' => $queryParams,
+                'data' => $data,
+            ],
+        ]);
     }
 }
